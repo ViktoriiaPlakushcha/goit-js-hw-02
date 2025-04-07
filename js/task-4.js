885bcac594916bc54b9277a6b836c88d378eb5ad
Вікторія Плакуща <dneprovaviktoria@gmail.com>
2025-04-07 19:55:51 +0300

    fix mistakes

--- js/task-4.js
+++ js/task-4.js
@@ -1,28 +1,27 @@
 function getShippingCost(country) {
-    const countryOne = 'China';
-    const countryTwo = 'Chile';
-    const countryThree = 'Australia';
-    const countryFour = 'Jamaica';
-    const priceOne = 100;
-    const priceTwo = 250;
-    const priceThree = 170;
-    const priceFour = 120;
+    let price;
+
     switch (country) {
-        case 'China':
-            return `Shipping to ${countryOne} will cost ${priceOne} credits`;
-            break;
-        case 'Chile':
-            return `Shipping to ${countryTwo} will cost ${priceTwo} credits`;
-            break;
-        case 'Australia':
-            return `Shipping to ${countryThree} will cost ${priceThree} credits`;
-            break;
-        case 'Jamaica':
-            return `Shipping to ${countryFour} will cost ${priceFour} credits`;
-            break;
-        default:
-            return `Sorry, there is no delivery to your country`
-    }
+    case 'China':
+    price = 100;
+    break;
+        
+    case 'Chile':
+    price = 250;
+    break;
+    
+    case 'Australia':
+    price = 170;
+    break;
+    
+    case 'Jamaica':
+    price = 120;
+    break;
+    
+    default:
+    return "Sorry, there is no delivery to your country";
+}
+return `Shipping to ${country} will cost ${price} credits`;
 }
 console.log(getShippingCost("Australia"));
 console.log(getShippingCost("Germany"));
